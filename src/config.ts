64fba366a79bca07b83a192180e08ba7a2f851cd
William-Mou <william.mou1024@gmail.com>
2023-06-18 21:36:22 +0800

    Added SITE_TITLE, SITE_DESCRIPTION and OG image

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'NTHU SCC | Student Cluster Competition Team';
+export const SITE_DESCRIPTION = 'We are the NTHU SCC, the Student Cluster Competition Team representing the National Tsing Hua University. Our primary objective is to attain championship in diverse student cluster competitions, thereby establishing ourselves as leaders in the field of supercomputing and high-performance computing (HPC). Operating under the esteemed Large-scale System Architecture Lab (LSA Lab) at National Tsing Hua University, our team strives to push the boundaries of innovation and excellence in this domain.';
