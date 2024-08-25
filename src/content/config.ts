d3be18a3504895cc3e3a7ca1289b493c1a6d2f27
catking <edarhinos9@gmail.com>
2024-08-25 23:55:15 +0800

    Update Website structure
    Add news and members pages. Also update events to new collection and create new content folder.

--- src/content/config.ts
+++ src/content/config.ts
@@ -31,16 +31,40 @@ const competitionsSchema = z.object({
     badge: z.string().optional(),
 });
 
+const eventsSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.string().optional(),
+    heroImage: z.string().optional(),
+    badge: z.string().optional(),
+});
+
+const newsSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.string().optional(),
+    heroImage: z.string().optional(),
+    badge: z.string().optional(),
+});
+
 export type BlogSchema = z.infer<typeof blogSchema>;
 export type StoreSchema = z.infer<typeof storeSchema>;
 export type CompetitionsSchema = z.infer<typeof competitionsSchema>;
+export type EventsSchema = z.infer<typeof eventsSchema>;
+export type NewsSchema = z.infer<typeof newsSchema>;
 
 const blogCollection = defineCollection({ schema: blogSchema });
 const storeCollection = defineCollection({ schema: storeSchema });
 const competitionsCollection = defineCollection({ schema: competitionsSchema });
+const eventsCollection = defineCollection({ schema: eventsSchema });
+const newsCollection = defineCollection({ schema: newsSchema });
 
 export const collections = {
     'blog': blogCollection,
     'store': storeCollection,
-    'competitions': competitionsCollection
+    'competitions': competitionsCollection,
+    'events': eventsCollection,
+    'news': newsCollection,
 }
---
+++ src/content/news/news_template.md
@@ -0,0 +1,39 @@
+---
+title: "News"
+description: "News description"
+pubDate: "Aug 25 2024"
+heroImage: "/social_img.jpg"
+badge: "NEW"
+---
+
+<!-- CSS Code: Place this code in the document's head (between the 'head' tags) -->
+<style>
+table.GeneratedTable {
+  width: 100%;
+  background-color: #ffffff;
+  border-collapse: collapse;
+  border-width: 2px;
+  border-color: #68624b;
+  border-style: solid;
+  color: #000000;
+}
+
+table.GeneratedTable td, table.GeneratedTable th {
+  border-width: 2px;
+  border-color: #68624b;
+  border-style: solid;
+  padding: 3px;
+}
+
+table.GeneratedTable thead {
+  background-color: #ffcc00;
+}
+
+.highlight-red-text {
+  color: #FF0000; /* 這是一個代表紅色的色碼，亮紅色 */
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5); /* 添加文字陰影 */
+}
+
+</style>
+
+#### 新聞測試
